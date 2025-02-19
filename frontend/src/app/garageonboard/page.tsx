f577d1022efd2e4387da90d6af3962e776ae2ad9
Expo-67 <145459052+Expo-67@users.noreply.github.com>
2025-02-19 21:12:30 +0300

    admin dashboard

---
+++ frontend/src/app/garageonboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+import brand2 from
+
+const garageOnboardpage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-white/90 to-gray-800 p-4">
+      
+    </div>
+  )
+}
+
+export default garageOnboardpage
